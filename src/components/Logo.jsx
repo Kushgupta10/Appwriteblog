164da68fdffd26be91e5c2aa30c4d6b08cb1a756
Kush Gupta <kushgupta9274@gmail.com>
2024-10-05 16:11:50 +0530

    first commit

---
+++ src/components/Logo.jsx
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../components/Images/logo.png'
+
+function Logo({width = '100px' }) {
+  return (
+    <div > Logo
+      {/* <img src={logo} alt='logo' /> */}
+    </div>
+  )
+}
+
+export default Logo
